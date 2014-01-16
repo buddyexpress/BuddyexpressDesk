e5ccd0f8080e0e738422900642193ed1b87e520a
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:11:59 +0500

    v1.0-leonard initial release

---
+++ system/action.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+require_once(dirname(dirname(__FILE__)).'/system/BuddyexpressDesk.php');
+buddyexpressdesk_action(input('action'));
