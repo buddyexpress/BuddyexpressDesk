89e7ac363ee804248a87669d808e1e4ae60a4371
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:12:04 +0500

    v1.0-leonard initial release

---
+++ templates/default/buddyexpressdesk_template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
