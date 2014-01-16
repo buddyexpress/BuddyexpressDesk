42d75fdbb46a2cd7be52b5481f3507cd6a741d8d
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:00:46 +0500

    v1.0-leonard initial release

---
+++ configurations/libraries.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+
+$BuddyexpressDesk->libraries = array(
+									 'css',
+									 'configs', 
+									 'system',
+									 'views',
+									 'articles',
+									 'actions',
+									 'languages',
+									 'menus',
+									 'admin',
+									 'default',
+									 'components',
+									 'bframework'
+									);
