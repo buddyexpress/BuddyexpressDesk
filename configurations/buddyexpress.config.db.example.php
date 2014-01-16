80f4a6e705decadd571ea56a3f488c13ea50fdd3
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:00:41 +0500

    v1.0-leonard initial release

---
+++ configurations/buddyexpress.config.db.example.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+
+// replace <<host>> with your database host name;
+$BuddyexpressDesk->host = '<<host>>';
+
+// replace <<user>> with your database username;
+$BuddyexpressDesk->user = '<<user>>';
+
+// replace <<password>> with your database password;
+$BuddyexpressDesk->password = '<<password>>';
+
+// replace <<dbname>> with your database name;
+$BuddyexpressDesk->database = '<<dbname>>';
