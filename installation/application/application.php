1a0a274601ea7132ab1ae765c85c462f5ff0c62f
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:01:49 +0500

    v1.0-leonard initial release

---
+++ installation/application/application.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+$BframeworkVersion = '1.0';
+$BframeworkRelease = 'January 2014';
+$BframeworkAppName = 'Installation';
+$BframeworkadminEmail = false;
