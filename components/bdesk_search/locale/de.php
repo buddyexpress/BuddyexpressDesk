a6b46409d24aeef27152a2ddb2da26407f0480da
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 22:59:49 +0500

    v1.0-leonard initial release

---
+++ components/bdesk_search/locale/de.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+$de = array(
+		   'search:search' => 'Suche',
+		   'search:result' => 'Suchergebnis für'
+			 );
+
+buddyexpressdesk_register_languages('de' , $de);
