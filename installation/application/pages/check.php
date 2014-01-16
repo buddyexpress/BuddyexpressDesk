ab639c6c68e5f51c1f4e6b6cda48fdf4e51e118d
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:03:56 +0500

    v1.0-leonard initial release

---
+++ installation/application/pages/check.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Bframework Application start file
+ *
+ * @package   Bframework
+ * @subpackage my application 
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2012 BUDDYEXPRESS.
+ * @ApplicationCopyright < Your Copyrights� >
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @ApplicationLicense < Your License >
+ * @link      http://www.buddyexpress.net
+ */
+
+echo bframework_view_page(array(
+'body' => bframework_view(bframework_get_approot_path().'template/check'),
+'title' => bframework_print('bdesk:check')
+));
