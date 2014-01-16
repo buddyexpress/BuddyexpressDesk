d242feaef83048ae964553389633410a5bf30984
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 22:59:31 +0500

    v1.0-leonard initial release

---
+++ admin/pages/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+$title = buddyexpressdesk_print('admin:admin');
+$content = array(
+				 'contents' =>  buddyexpressdesk_view(buddyexpressdesk_route()->admin.'pages/contents/index')
+			);
+echo buddyexpressdesk_view_page($title, buddyexpressdesk_layout_view('article', $content), 'administrator');
