eb9ec7bc8076adb9bfcfd37575e2d470037fb69d
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 22:59:22 +0500

    v1.0-leonard initial release

---
+++ admin/pages/contents/login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+
+echo buddyexpressdesk_form(array(
+								 'body' => buddyexpressdesk_view_form('account/login'),
+								 'attributes' => array('action' => buddyexpressdesk_site_url().'action/login', 'method' => 'POST')
+								 ));
