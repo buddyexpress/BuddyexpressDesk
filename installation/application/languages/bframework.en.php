60f749d1c76c1d9a0165c8cebae4138d15c5a59f
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:03:53 +0500

    v1.0-leonard initial release

---
+++ installation/application/languages/bframework.en.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+
+$englsih = array(
+     'bedsk' => 'Settings',
+	 'bdesk:installed' => 'Installed',
+	 'bdesk:installation' => 'Installation',
+	 'bdesk:check' => 'Validate',
+	 'bdesk:installed' => 'Installed',
+	 'bdesk:installed:message' => 'BuddyexpressDesk has been installed. After login please change login email and passsword!',
+	 'bdesk:installation' => 'Installation',
+     'bdesk:settings' => 'Settings',
+	 'bdesk:login:details' => 'Login Details',
+	 'bdesk:login:email' => '<strong>Email: </strong> buddydesk@buddyexpress.net ',
+	 'bdesk:login:password' => '<strong>Password: </strong> administrator ',
+	 'bdesk:dbuser' => 'Database User',
+ 	 'bdesk:password' => 'Database Password',
+	 'bdesk:dbname' => 'Database Name',
+	 'bdesk:dbhost' => 'Database Host',
+	 'bdesk:weburl' => 'BuddyexpressDesk Url',
+
+);
+
+bframework_register_languages($englsih);
+?>
