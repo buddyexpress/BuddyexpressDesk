c69e9e288256eac63622174aeb649e982dc8c6f3
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:00:29 +0500

    v1.0-leonard initial release

---
+++ configurations/buddyexpress.site.config.example.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+
+/**
+* BuddyexpressDesk need a website url, 
+* Please make sure you have enter backslash '/' at the end of url
+* like http://www.mywebsite.com <= this is not accepted
+* url like http://www.mywebsite.com/ <= this is accepted
+*/
+$BuddyexpressDesk->url = '<<url>>';
+
+/* 
+* BuddyexpressDesk display errors or not, 
+* Enter on or off
+* Example ini_set('display_errors','off'); 
+*/
+ini_set('display_errors','off'); 
