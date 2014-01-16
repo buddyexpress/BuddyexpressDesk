6e80fe4ef515d1c8802c805aea5bf74f1c42cba2
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:00:32 +0500

    v1.0-leonard initial release

---
+++ components/besk_editor/head/editor.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+?>
+<script src="<?php echo buddyexpressdesk_site_url();?>vendors/tinymce/tinymce.min.js"></script>
+<script>tinymce.init({selector:'textarea',max_height: 200,min_height: 160,height : 180,plugins: "code"});</script>   
