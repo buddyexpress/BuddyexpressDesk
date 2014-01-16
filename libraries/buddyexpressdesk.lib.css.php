f6143d3a24f7fdea94dc603f7de4b5a5575b5473
arsalanshah <arsalan@buddyexpress.net>
2014-01-16 23:10:57 +0500

    v1.0-leonard initial release

---
+++ libraries/buddyexpressdesk.lib.css.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Buddyexpress Desk
+ *
+ * @package   Bdesk
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2014 BUDDYEXPRESS NETWORKS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://labs.buddyexpress.net/bdesk.b
+ */
+ /**
+* Include css in pages;
+* @last edit: $arsalanshah
+* @Reason: Initial;
+* 
+*/ 
+function buddyexpressdesk_css($path){
+if(isset($path) && !empty($path)){
+    $style ='<link rel="stylesheet" href="'.$path.'" type="text/css"/>';
+	return $style;
+  }
+	
+}
