4c233d1a3f806ec8bce83b4f865955dc87e8cf3f
arsalanshah <arsalan@buddyexpress.net>
2014-01-28 20:18:52 +0500

    update class

--- classes/BuddyexpressDesk.Components.php
+++ classes/BuddyexpressDesk.Components.php
@@ -8,7 +8,6 @@
  * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
  * @link      http://labs.buddyexpress.net/bdesk.b
  */
-
 class BUDDYEXPRESS_DESK_COM {
 /**
 * BuddyDesk get list;
@@ -16,7 +15,7 @@ class BUDDYEXPRESS_DESK_COM {
 * @Reason: Initial;
 * 
 */	
-public function getList(){
+public static function getList(){
 	
     $dir = buddyexpressdesk_route()->com;
 	$components_ids = array();
@@ -45,7 +44,7 @@ public function getList(){
 * @Reason: Initial;
 * 
 */
-public function getCOM($name){
+public static function getCOM($name){
 	$name = trim($name);
  if(!preg_match('/\s/',$name)){
        $dir = buddyexpressdesk_route()->com;
@@ -62,7 +61,7 @@ return false;
 * @Reason: Initial;
 * 
 */
-public function ENABLE($com){
+public static function ENABLE($com){
 if(!empty($com) && is_dir(buddyexpressdesk_route()->com.$com)){	
 	$ENABLE = new BDESK_DB;
 	/**
@@ -70,7 +69,10 @@ if(!empty($com) && is_dir(buddyexpressdesk_route()->com.$com)){
 	* @last edit: $arsalanshah
     * @Reason: Initial;
 	*/
-	$ENABLE->statement("SELECT * FROM bdesk_components WHERE (com_id='$com')");
+	$ENABLE->statement("SELECT * 
+					    FROM bdesk_components 
+					    WHERE (com_id='$com');"
+					   );
 	$ENABLE->execute();
 	$CHECK = $ENABLE->fetch();
 	/**
@@ -80,7 +82,10 @@ if(!empty($com) && is_dir(buddyexpressdesk_route()->com.$com)){
 	*/
     if(isset($CHECK['active']) && $CHECK['active'] == 0){
 	$ENABLE = new BDESK_DB;	
-	$ENABLE->statement("UPDATE bdesk_components SET active='1' WHERE (com_id='$com')");
+	$ENABLE->statement("UPDATE bdesk_components 
+			           SET active='1' 
+					   WHERE (com_id='$com');"
+					   );
 	$ENABLE->execute();
 	return true;
 	} 
@@ -91,7 +96,10 @@ if(!empty($com) && is_dir(buddyexpressdesk_route()->com.$com)){
 	* @last edit: $arsalanshah 
     * @Reason: Initial;
 	*/
-	$ENABLE->statement("INSERT INTO `bdesk_components` (`com_id`, `active`) VALUES ('$com', '1')");
+	$ENABLE->statement("INSERT INTO `bdesk_components` 
+					   (`com_id`, `active`) 
+		        VALUES ('$com', '1')"
+				);
 	$ENABLE->execute();
 	return true;
 	}
@@ -108,7 +116,9 @@ return false;
 public function CHECK($com = NULL){
 if(!empty($com)){	
 	$ENABLE = new BDESK_DB;
-	$ENABLE->statement("SELECT * FROM bdesk_components WHERE (com_id='$com')");
+	$ENABLE->statement("SELECT * FROM bdesk_components 
+					   WHERE (com_id='$com')"
+					   );
 	$ENABLE->execute();
 	$CHECK = $ENABLE->fetch();
 	if(isset($CHECK['active']) && $CHECK['active'] == 1){
@@ -130,7 +140,9 @@ return false;
 public function DISABLE($com = NULL){
 if(!empty($com)){	
 	$DISABLE = new BDESK_DB;
-	$DISABLE->statement("UPDATE bdesk_components SET active='0' WHERE (com_id='$com')");
+	$DISABLE->statement("UPDATE bdesk_components 
+						SET active='0' WHERE (com_id='$com')"
+						);
 	$DISABLE->execute();
 	return true;
 }
@@ -145,7 +157,10 @@ return false;
 */
 public function getActive(){
 	$GET = new BDESK_DB;
-    $GET->statement("SELECT * FROM bdesk_components WHERE (active=1)");
+    $GET->statement("SELECT * 
+					FROM bdesk_components 
+					WHERE (active=1);"
+					);
 	$GET->execute();
 	$GET = $GET->fetch(true);
 	if($GET){
@@ -184,5 +199,127 @@ public static function BuddyDesk_Version(){
 	   }
 	return false;   
 }
+/**
+* BuddyDesk Register Component Settings
+* @params : $com = name of com
+* @last edit: $arsalanshah
+* @Reason: Initial;
+* 
+*/
+public static function registerSettings($name, $file_func){
+   global  $BuddyexpressDesk;
+   if(!empty($name) 
+			 && !empty($file_func) 
+			 && is_dir(buddyexpressdesk_route()->com.$name)){
+   $actions = $BuddyexpressDesk->settings[$name] = $file_func;
+   return $actions;	
+   }
+  return false; 
+}
+/**
+* Get all the componenets that have settings
+* @params : Null
+* @last edit: $arsalanshah
+* @Reason: Initial;
+* 
+*/
+public static function WithSettings(){
+   global  $BuddyexpressDesk;
+   if(!empty($BuddyexpressDesk->settings)){
+   return $BuddyexpressDesk->settings;	
+   }
+  return false; 
+}
+/**
+* Get a settings form for componenet
+* @params : $form = name of form
+* @last edit: $arsalanshah
+* @Reason: Initial;
+* 
+*/
+public static function getForm($form, $params = array()){
+if(!empty($form) && is_callable($form)){	
+   ob_start();
+   call_user_func($form, $params);
+   $form = ob_get_clean();
+   return $form;
+}
 
 }
+/**
+* BuddyDesk Save settings for component
+* @params : array();
+* @last edit: $arsalanshah
+* @Reason: Initial;
+* 
+*/
+public static function SaveSettings($params = array()){
+	if(!isset($params['com']) || isset($params['field'])){
+	 exit;	
+	}
+	$com = $params['com'];
+	$field = $params['field'];
+	$value = $params['value'];
+	if(empty($com) || empty($field)){
+	  exit;	
+	}
+	$GET = new BDESK_DB;
+	$GET->statement("SELECT * 
+					FROM bdesk_components_settings 
+		            WHERE (com_id='$com' AND field='$field')"
+					);
+	$GET->execute();
+	$GET = $GET->fetch();
+	
+	/**
+	* Insert if settings are empty
+	* Require: $this{settings}
+	* @lastupdat: $arsalanshah
+	*/
+	if(empty($GET)){
+	$SAVE = new BDESK_DB;
+	$SAVE->statement("INSERT INTO bdesk_components_settings 
+					 (`com_id`, `field`, `value`) 
+			  VALUES ('$com', '$field', '$value');"
+			   );
+	$SAVE->execute();
+	} 
+	/**
+	* Update if settings exisits
+	* Require: $this{settings}
+	* @lastupdat: $arsalanshah
+	*/
+	elseif(!empty($GET['field'])){
+    $SET = new BDESK_DB;
+	$SET->statement("UPDATE bdesk_components_settings 
+					SET com_id='$com' , field='$field', value='$value' 
+				  WHERE(com_id='$com' AND field='$field');"
+				  );
+	$SET->execute();		
+	}
+}
+/**
+* BuddyDesk Save settings for component
+* @params : array();
+* @last edit: $arsalanshah
+* @Reason: Initial;
+* 
+*/
+public static function getSettings($com, $field){
+	if(empty($com) || empty($field)){
+	  exit;	
+	}
+	$GET = new BDESK_DB;
+	$GET->statement("SELECT * 
+					FROM bdesk_components_settings 
+		            WHERE (com_id='$com' AND field='$field')"
+					);
+	$GET->execute();
+	$GET = $GET->fetch();
+	if(!empty($GET)){
+	    return $GET['value'];	
+	}
+return false;	
+}
+
+}//class
