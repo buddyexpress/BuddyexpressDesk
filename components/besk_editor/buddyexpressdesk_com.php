caaee35a419f50d2038fb2bc4f208243b8eb9435
unknown <Arsalan Shah@arsalan-PC.(none)>
2014-02-13 20:23:57 +0500

    Allow base url

--- components/besk_editor/buddyexpressdesk_com.php
+++ components/besk_editor/buddyexpressdesk_com.php
@@ -12,3 +12,9 @@
 $plugin = 'besk_editor';
 define('COM_EDITOR_WWW', buddyexpressdesk_route()->com.$plugin);
 buddyexpressdesk_register_view('BuddyexpressDesk/page/admin/head', COM_EDITOR_WWW.'/head/editor.php');
+buddyexpressdesk_register_view('BuddyexpressDesk/page/admin/head/js', 'base_url_bdesk_editor');
+
+function base_url_bdesk_editor($context, $params = false, $url){ 
+ $baseurl = buddyexpressdesk_site_url();	
+ return 'bdesk_edtior_url = "'.$baseurl.'"';
+}
