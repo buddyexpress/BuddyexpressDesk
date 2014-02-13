7cdb30f1e9d858b486cb5144e90fcc9d3b6104c6
unknown <Arsalan Shah@arsalan-PC.(none)>
2014-02-13 20:23:34 +0500

    allow to extend aticle add page

--- admin/pages/contents/articles/add.php
+++ admin/pages/contents/articles/add.php
@@ -13,3 +13,4 @@ echo buddyexpressdesk_form(array(
 								 'body' => buddyexpressdesk_view_form('articles/add'),
 								 'attributes' => array('action' => buddyexpressdesk_site_url().'action/articleadd', 'method' => 'POST')
 								 ));
+echo buddyexpressdesk_fetch_views('BuddyexpressDesk/article/add'); 
