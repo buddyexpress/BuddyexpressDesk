7843ee11de81f0d841ab16ba4ceecc2fb83adde3
arsalanshah <arsalan@buddyexpress.net>
2014-02-20 23:53:03 +0500

    #1

--- pages/contents/articles/view.php
+++ pages/contents/articles/view.php
@@ -13,7 +13,7 @@ $article = $params['article'];
 $owner = get_user_by_uid($article['owner_id']);
 echo '<h2>'.$article['title'].'</h2>';
 echo '<p>'.generateTitle($article['article']).'</p>';
-echo '<div class="posted" style="font-size: 12px;color: #4E4E4E;">';
+echo '<div class="posted article-view-owner" >';
 echo 'Posted by: '.$owner['name'].' '.$article['time'].'</div>';
 unset($owner['password']);
 echo '<br />';
